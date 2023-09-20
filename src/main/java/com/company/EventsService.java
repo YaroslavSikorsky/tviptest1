e89ed51a453ccf794aede8384af584be731e0172
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-09-20 21:41:20 +0300

    Update

--- src/main/java/com/company/EventsService.java
+++ src/main/java/com/company/EventsService.java
@@ -3,15 +3,16 @@ package com.company;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class EventsService {
-	private List<CustomEvent> eventCache;
+	private final List<CustomEvent> eventCache;
 	private final Duration expiration = Duration.ofDays(1);
 
-	public EventsService(List<CustomEvent> event) {
-		this.eventCache = event;
+	public EventsService() {
+		this.eventCache = new ArrayList<>();
 	}
 
 	private boolean isEventValid(CustomEvent event) {
@@ -25,6 +26,7 @@ public class EventsService {
 	public boolean put(CustomEvent event) {
 		if (isEventValid(event)) {
 			eventCache.add(event);
+//			System.out.println(eventCache.get(0).getResource().getIds().toString());
 			return true;
 		} else {
 			System.out.println("Invalid event: " + event);
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,17 +1,39 @@
 package com.company;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.time.Instant;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("start");
 
-		CustomEvent customEvent = new CustomEvent(Instant.ofEpochSecond(1695006509000), new Resource("CONTENT", List.of("1", "5", "9"));
-		CustomEvent customEvent2 = new CustomEvent(Instant.ofEpochSecond(1695006509000), new Resource("USER", List.of("59011", "15", "1695190408876"));
+		CustomEvent customEvent = new CustomEvent(Instant.ofEpochMilli(1695006509000L), new Resource("CONTENT", List.of("1", "5", "9")));
+		CustomEvent customEvent2 = new CustomEvent(Instant.ofEpochMilli(1695006509000L), new Resource("USER", List.of("59011", "15", "1695190408876")));
 
-		EventsService eventsService = new EventsService(List.of(customEvent, customEvent2));
+		EventsService eventsService = new EventsService();
 
-		//ObjectMapper.writeValue();
+		boolean event1Result = eventsService.put(customEvent);
+		boolean event2Result = eventsService.put(customEvent2);
+
+		System.out.println("Event1 result: " + event1Result); // true
+		System.out.println("Event2 result: " + event2Result); // true
+
+		List<CustomEvent> events = eventsService.events();
+		ObjectMapper objectMapper = new ObjectMapper();
+
+
+		for (CustomEvent event : events) {
+			String eventJson = "";
+			try {
+				eventJson = objectMapper.writeValueAsString(event);
+				System.out.println(eventJson);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			} finally {
+				System.out.println("Event: " + eventJson);
+			}
+		}
 	}
 }
