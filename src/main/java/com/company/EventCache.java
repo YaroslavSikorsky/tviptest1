5cce4fe9ac4692bd041d3fe9e02f7fedbdd1a231
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-09-29 21:41:02 +0300

    added locks

--- src/main/java/com/company/EventCache.java
+++ src/main/java/com/company/EventCache.java
@@ -4,35 +4,48 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
+
 
 public class EventCache {
 	private final List<CustomEvent> eventCache = new ArrayList<>();
 	private final Duration expiration;
 	private static final Logger logger = Logger.getLogger(EventCache.class.getName());
+	private final Lock cacheLock = new ReentrantLock();
 
 	public EventCache(Duration expiration) {
 		this.expiration = expiration;
 	}
 
 	public boolean put(CustomEvent event) {
-		if (EventValidator.isEventValid(event)) {
-			eventCache.add(event);
-			logger.info("Event added: " + event);
-			return true;
-		} else {
-			logger.info("Invalid event: " + event);
-			return false;
+		try {
+			cacheLock.lock();
+			if (EventValidator.isEventValid(event)) {
+				eventCache.add(event);
+				logger.info("Event added: " + event);
+				return true;
+			} else {
+				logger.info("Invalid event: " + event);
+				return false;
+			}
+		} finally {
+			cacheLock.unlock();
 		}
 	}
 
 	public List<CustomEvent> getValidEvents() {
-		Instant currentTimestamp = Instant.now();
-		List<CustomEvent> validEvents = eventCache.stream()
-				.filter(event -> Duration.between(event.getTimestamp(), currentTimestamp).compareTo(expiration) <= 0)
-				.toList();
-		logger.info("Retrieved valid events: " + validEvents.size());
-		return validEvents;
+		try {
+			cacheLock.lock();
+			Instant currentTimestamp = Instant.now();
+			List<CustomEvent> validEvents = eventCache.stream()
+					.filter(event -> Duration.between(event.getTimestamp(), currentTimestamp).compareTo(expiration) <= 0)
+					.toList();
+			logger.info("Retrieved valid events: " + validEvents.size());
+			return validEvents;
+		} finally {
+			cacheLock.unlock();
+		}
 	}
-
 }
--- src/test/java/com/company/EventServiceTest.java
+++ src/test/java/com/company/EventServiceTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class EventServiceTest {
-	private final EventServiceWrapper eventServiceWrapper = new EventServiceWrapper(new EventService(Duration.ofHours(211)));
+	private final EventServiceWrapper eventServiceWrapper = new EventServiceWrapper(new EventService(Duration.ofHours(300)));
 	private static final Logger logger = Logger.getLogger(EventServiceTest.class.getName());
 
 	@Test()
