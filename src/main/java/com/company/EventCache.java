15c0f1f6d60359e39fb9be779a4e79b3ed87a795
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-09-30 20:16:56 +0300

    added ReentrantReadWriteLock

--- src/main/java/com/company/EventCache.java
+++ src/main/java/com/company/EventCache.java
@@ -4,8 +4,8 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Logger;
 
 
@@ -13,15 +13,15 @@ public class EventCache {
 	private final List<CustomEvent> eventCache = new ArrayList<>();
 	private final Duration expiration;
 	private static final Logger logger = Logger.getLogger(EventCache.class.getName());
-	private final Lock cacheLock = new ReentrantLock();
+	private final ReadWriteLock cacheLock = new ReentrantReadWriteLock();
 
 	public EventCache(Duration expiration) {
 		this.expiration = expiration;
 	}
 
 	public boolean put(CustomEvent event) {
+		cacheLock.writeLock().lock();
 		try {
-			cacheLock.lock();
 			if (EventValidator.isEventValid(event)) {
 				eventCache.add(event);
 				logger.info("Event added: " + event);
@@ -31,13 +31,13 @@ public class EventCache {
 				return false;
 			}
 		} finally {
-			cacheLock.unlock();
+			cacheLock.writeLock().unlock();
 		}
 	}
 
 	public List<CustomEvent> getValidEvents() {
+		cacheLock.readLock().lock();
 		try {
-			cacheLock.lock();
 			Instant currentTimestamp = Instant.now();
 			List<CustomEvent> validEvents = eventCache.stream()
 					.filter(event -> Duration.between(event.getTimestamp(), currentTimestamp).compareTo(expiration) <= 0)
@@ -45,7 +45,7 @@ public class EventCache {
 			logger.info("Retrieved valid events: " + validEvents.size());
 			return validEvents;
 		} finally {
-			cacheLock.unlock();
+			cacheLock.readLock().unlock();
 		}
 	}
 }
